92f1f7a33a06b9937c0a0a544ffce40f13e137cc
João Malvino Júnior <jmalvino77@gmail.com>
2022-10-27 15:02:39 -0300

    criacao da pagina de template e  o index dos 2 app

--- medicos/views.py
+++ medicos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Pagina de Medicos')
+    return render(request,'medicos/index.html')
--- pacientes/views.py
+++ pacientes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Pagina de Pacientes')
+    return render (request, 'pacientes/index.html')
