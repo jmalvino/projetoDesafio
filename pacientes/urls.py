38bb70b80981534d2444e7710f2b595d0d39c3cc
João Malvino Júnior <jmalvino77@gmail.com>
2022-10-29 13:06:55 -0300

    criando pagina e metos de criacao de pacientes

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import PacientesCreate
+
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('cadastrar', PacientesCreate.as_view(), name="cadastar-pacientes")
 ]
--- pacientes/views.py
+++ pacientes/views.py
@@ -1,5 +1,15 @@
+from audioop import reverse
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Pacientes
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+class PacientesCreate(CreateView):
+    model = Pacientes
+    fields = ['nome','telefone','endereco','numero','cidade','uf','pais','cep']
+    template_name: 'pacientes/form.html'
+    success_url = reverse_lazy('index')
 
 def index(request):
-    return render (request, 'pacientes/index.html')
+    pacientes = Pacientes.objects.all()
+    return render (request, 'pacientes/index.html', {'pacientes':pacientes})
