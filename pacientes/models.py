e75e65d61a9f5f82c2e27d3635fa11a743c7747a
João Malvino Júnior <jmalvino77@gmail.com>
2022-10-29 10:25:36 -0300

    makemigrations e migrate

--- pacientes/models.py
+++ pacientes/models.py
@@ -5,14 +5,14 @@ from datetime import datetime
 
 class Pacientes(models.Model):
     nome = models.CharField(max_length=255)
-    telefone = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=20)
     endereco = models.CharField(max_length=255)
-    numero = models.CharField(max_length=50)
+    numero = models.CharField(max_length=10)
     cidade = models.CharField(max_length=100)
     uf = models.CharField(max_length=2)  
     pais = models.CharField(max_length=100)
     cep = models.CharField(max_length=8)
-    dataDeCriacao = models.DateTimeField()
+    dataDeCriacao = models.DateTimeField(auto_now_add=True)
 
 
 #metodo que mostra no banco de dados o nome definido
